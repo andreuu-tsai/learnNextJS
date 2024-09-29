6748692f67c291e16b735dd4f1221917473f3c10
Andrew Tsai <a56342003@gmail.com>
2024-09-30 00:01:45 +0800

    style: update indentation to 2 spaces and standardize quotation marks
    - Changed indentation to 2 spaces across all files for consistency.
    - Updated all single quotes to double quotes to standardize code style.

--- homework/0_TodoList/todo-list-react/src/components/TodoList.js
+++ homework/0_TodoList/todo-list-react/src/components/TodoList.js
@@ -1,95 +1,95 @@
 import React from "react";
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from "uuid";
 
 function TodoList() {
-    const [todoMap, setTodoMap] = React.useState(new Map());
-    const [todoOrder, setTodoOrder] = React.useState([]);
-    const [inputText, setInputText] = React.useState("");
+  const [todoMap, setTodoMap] = React.useState(new Map());
+  const [todoOrder, setTodoOrder] = React.useState([]);
+  const [inputText, setInputText] = React.useState("");
 
-    function addTodo(name) {
-        if (name.trim() === "") return  
-        const id = uuidv4();
-        let newTodo = {
-            id: id,
-            name: name,
-            isDone: false
-        };
-        setTodoMap(prevMap => new Map(prevMap).set(id, newTodo));
-        setTodoOrder(l => [id, ...l]);
-        setInputText("")
-    }
+  function addTodo(name) {
+    if (name.trim() === "") return;
+    const id = uuidv4();
+    let newTodo = {
+      id: id,
+      name: name,
+      isDone: false,
+    };
+    setTodoMap((prevMap) => new Map(prevMap).set(id, newTodo));
+    setTodoOrder((l) => [id, ...l]);
+    setInputText("");
+  }
 
-    function deleteHandler(id){
-        setTodoMap(prevMap => {
-            const newMap = new Map(prevMap);
-            newMap.delete(id);
-            return newMap;
-        })
-        setTodoOrder(prevOrder => prevOrder.filter( i => i !== id))
-    }
+  function deleteHandler(id) {
+    setTodoMap((prevMap) => {
+      const newMap = new Map(prevMap);
+      newMap.delete(id);
+      return newMap;
+    });
+    setTodoOrder((prevOrder) => prevOrder.filter((i) => i !== id));
+  }
 
-    function doneHandler(id) {
-        setTodoMap(prevMap => {
-            const newMap = new Map(prevMap);
-            const todo = newMap.get(id);
-            newMap.set(id, {...todo, isDone: !todo.isDone})
-            return newMap;
-        })
-    }
+  function doneHandler(id) {
+    setTodoMap((prevMap) => {
+      const newMap = new Map(prevMap);
+      const todo = newMap.get(id);
+      newMap.set(id, { ...todo, isDone: !todo.isDone });
+      return newMap;
+    });
+  }
 
-    function updateNameHandler(id, newName) {
-        if (newName.trim() === "") return;
-        setTodoMap(prevMap => {
-            const newMap = new Map(prevMap);
-            const todo = newMap.get(id);
-            newMap.set(id, {...todo, name: newName})
-            return newMap;
-        })
-    }
+  function updateNameHandler(id, newName) {
+    if (newName.trim() === "") return;
+    setTodoMap((prevMap) => {
+      const newMap = new Map(prevMap);
+      const todo = newMap.get(id);
+      newMap.set(id, { ...todo, name: newName });
+      return newMap;
+    });
+  }
 
-    return (
-        <div>
-            <h1>Todo List</h1>
-            <input 
-                type="text" 
-                value={inputText} 
-                onChange={(e) => {
-                    setInputText(e.target.value);
-                }}
-                placeholder="Something to do"
-            />
-            <button onClick={() => addTodo(inputText)}>Add</button>
-            {todoOrder.map(id => (
-                <Entry 
-                    key={id} 
-                    todo={todoMap.get(id)} 
-                    deleteHandler={deleteHandler}
-                    doneHandler={doneHandler}
-                    updateNameHandler={updateNameHandler}
-                />
-            ))}
-        </div>
-    )
-
-
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={inputText}
+        onChange={(e) => {
+          setInputText(e.target.value);
+        }}
+        placeholder="Something to do"
+      />
+      <button onClick={() => addTodo(inputText)}>Add</button>
+      {todoOrder.map((id) => (
+        <Entry
+          key={id}
+          todo={todoMap.get(id)}
+          deleteHandler={deleteHandler}
+          doneHandler={doneHandler}
+          updateNameHandler={updateNameHandler}
+        />
+      ))}
+    </div>
+  );
 }
 
-function Entry({todo, deleteHandler, doneHandler, updateNameHandler}){
-    return (
-        <div>
-            <input 
-                type="text" 
-                value={todo.name}
-                onChange={e => updateNameHandler(todo.id, e.target.value)}
-                style={{ 
-                    textDecoration: todo.isDone ? "line-through" : "none",
-                    color: todo.isDone ? "#888" : "black"
-                 }}  
-            />
-            <button onClick={() => doneHandler(todo.id)}>{todo.isDone ? "Undo":"Done"}</button>
-            <button onClick={() => deleteHandler(todo.id)}>Delete</button>
-        </div>
-    )
+function Entry({ todo, deleteHandler, doneHandler, updateNameHandler }) {
+  return (
+    <div>
+      <input
+        type="text"
+        value={todo.name}
+        onChange={(e) => updateNameHandler(todo.id, e.target.value)}
+        style={{
+          textDecoration: todo.isDone ? "line-through" : "none",
+          color: todo.isDone ? "#888" : "black",
+        }}
+      />
+      <button onClick={() => doneHandler(todo.id)}>
+        {todo.isDone ? "Undo" : "Done"}
+      </button>
+      <button onClick={() => deleteHandler(todo.id)}>Delete</button>
+    </div>
+  );
 }
 
-export default TodoList
+export default TodoList;
