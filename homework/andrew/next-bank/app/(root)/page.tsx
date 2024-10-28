f488399990d75fd8b167364afdaae33f16200b50
Andrew Tsai <a56342003@gmail.com>
2024-10-28 21:48:23 +0800

    Implement initial layouts and add UI components
    - Add sign-in, sign-up, my-banks, payment-transfer, and transaction-history pages
    - Update root layout with Sidebar and MobileNav
    - Add components: AnimatedCounter, BankCard, DoughnutChart, HeaderBox, MobileNav, RightSidebar, Sidebar, TotalBalanceBox

--- homework/andrew/next-bank/app/(root)/page.tsx
+++ homework/andrew/next-bank/app/(root)/page.tsx
@@ -1,3 +1,37 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import RightSidebar from "@/components/RightSidebar";
+
 export default function Home() {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: "Andrew",
+    lastName: "Tsai",
+    email: "andrew@company.com",
+  };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header>
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions effifiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        Recent Transaction
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500.5 }]}
+      />
+    </section>
+  );
 }
